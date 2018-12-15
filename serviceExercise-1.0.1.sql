83c37415e93e57c16e1f8c24df8077db8ee74522
LokeshRVLB <41204147+LokeshRVLB@users.noreply.github.com>
2018-12-15 18:09:15 +0530

    Add files via upload

--- serviceExercise-1.0.1.sql
+++ serviceExercise-1.0.1.sql
@@ -1,36 +1,41 @@
+use  lokesh_rajendran;
 DROP TABLE scv_person;
 DROP TABLE scv_address;
-
 CREATE TABLE scv_address(id BIGINT(20) AUTO_INCREMENT
-						  ,street VARCHAR(100) NOT NULL
-						  ,city VARCHAR(15) NOT NULL
-						  ,postal_code INT(11) NOT NULL
-						  ,PRIMARY KEY(id));
-
-
+							,street VARCHAR(100) NOT NULL
+							,city VARCHAR(15) NOT NULL
+							,postal_code INT(11) NOT NULL
+							, PRIMARY KEY(id));
 CREATE TABLE scv_person(id BIGINT(20) AUTO_INCREMENT
-						 	  ,first_name VARCHAR(50) NOT NULL
-						 	  ,last_name VARCHAR	(50) NOT NULL
-							  ,email VARCHAR(100) NOT NULL UNIQUE
-						 	  ,address_id BIGINT(20)
-					   	  ,dob DATE NOT NULL
-						 	  ,PRIMARY kEY(id)
-						 	  ,CONSTRAINT fk_sv_person_sv_address 
-						 		FOREIGN KEY(address_id)
-						  		REFERENCES scv_address(id)
-						  		ON DELETE CASCADE);
-						  
+							 	,first_name VARCHAR(50) NOT NULL
+							 	,last_name VARCHAR	(50) NOT NULL
+								,email VARCHAR(100) NOT NULL UNIQUE
+							 	,address_id BIGINT(20)
+						 	,dob DATE NOT NULL
+							 	, PRIMARY KEY(id)
+							 	, CONSTRAINT fk_sv_person_sv_address FOREIGN KEY(address_id) REFERENCES scv_address(id) ON
+DELETE CASCADE);
+							
 INSERT INTO scv_address(street, city, postal_code) VALUE ("Hassan Street", "chittoor", 517001);
 INSERT INTO scv_address(street, city, postal_code) VALUE ("Ragava street", "chennai", 600113);
 INSERT INTO scv_address(street, city, postal_code) VALUE ("Rajendran street", "chennai", 600114);
 
-INSERT INTO scv_person(first_name, last_name, email,  address_id, dob) VALUE ("Lokesh", "Balaji", "lokeshbalaji68@gmail.com", 1, '1996-08-06');
-INSERT INTO scv_person(first_name, last_name, email,  address_id, dob) VALUE ("Lokesh2", "Balaji2", "lokeshbalaji682@gmail.com", 2, '1996-08-06');
-INSERT INTO scv_person(first_name, last_name, email,  address_id, dob) VALUE ("Lokesh4", "Balaji4", "lokeshbalaji684@gmail.com", 3, '1996-08-06');
+INSERT INTO scv_person(first_name, last_name, email,  address_id, dob) VALUE ("Lokeshext", "Balaji", "lokeshbalaji68ext@gmail.com", 1, '1996-08-06');
+INSERT INTO scv_person(first_name, last_name, email,  address_id, dob) VALUE ("Lokeshext2", "Balaji2", "lokeshbalaji68ext2@gmail.com", 2, '1996-08-06');
+INSERT INTO scv_person(first_name, last_name, email,  address_id, dob) VALUE ("Lokeshext4", "Balaji4", "lokeshbalaji68ext4@gmail.com", 3, '1996-08-06');
 
 SELECT * FROM scv_person;
 SELECT * FROM scv_address;
 
+select count(email) from scv_person where id in (1,2);
+SELECT scv_person.id                
+      ,scv_person.first_name        
+      ,scv_person.last_name         
+      ,scv_person.email             
+      ,scv_person.dob               
+      ,scv_person.address_id        
+FROM scv_person                     
+WHERE scv_person.address_id IN ( 2,3 );
 
 UPDATE scv_person LEFT JOIN scv_address ON scv_person.address_id = scv_address.id 
 SET scv_person.first_name = "updated lokesh"
@@ -43,3 +48,15 @@ WHERE scv_person.id = 1;
 
 delete from scv_person,scv_address using scv_person inner join scv_address on scv_person.address_id = scv_address.id
 where scv_person.id = 1;
+
+DROP table person_admin;
+DROP table person_credentials;
+CREATE TABLE person_admin(email VARCHAR(100) NOT NULL UNIQUE);
+INSERT INTO person_admin VALUES ("lokesh@gmail.com");
+
+CREATE TABLE person_credentials(email VARCHAR(100) NOT NULL UNIQUE, pass VARCHAR(50) NOT NULL);
+INSERT INTO person_credentials values ("lokesh@gmail.com","lokesh@1234"), ("lokeshbalaji68@gmail.com", "lokesh1234");
+
+
+
+select * from person_credentials;
